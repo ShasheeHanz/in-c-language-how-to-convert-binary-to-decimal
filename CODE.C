48fc6e53266dc1cc255917cad810777684181a87
Shashee Hanz <52167316+spicaSL@users.noreply.github.com>
2019-10-23 10:26:43 +0530

    Update CODE.C

--- CODE.C
+++ CODE.C
@@ -1,1 +1,13 @@
-void main() { int num,binary_val,decimal_val =0, base= 1,rem; printf("Enter a binary number(1s and 0s) \n"); scanf("%d" , &num); /* maximum five digit */ binary_val =num; while (num > 0) { rem = num % 10; decimal_val = decimal_val + rem * base; num = num /10; base = base * 2; } printf("The Binary Number is =%d \n", binary_val); printf("Its Decimal Equivalent is = %d \n", decimal_val); }
+void main()
+{
+  int num,binary_val,decimal_val =0, base= 1,rem;
+  printf("Enter a binary number(1s and 0s) \n");
+  scanf("%d" , &num);
+  /* maximum five digit */
+  binary_val =num; while (num > 0)
+  {
+    rem = num % 10; decimal_val = decimal_val + rem * base; num = num /10; base = base * 2;
+  }
+  printf("The Binary Number is =%d \n", binary_val);
+  printf("Its Decimal Equivalent is = %d \n", decimal_val);
+}
